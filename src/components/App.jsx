3331bf958238d4c3021cc0ae8f16e39f8707508d
ragedteddy <55912131+ragedteddy@users.noreply.github.com>
2021-12-31 09:03:47 +0530

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,41 +3,41 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import TipsAndUpdatesIcon from "@material-ui/icons/TipsAndUpdates";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-  function handleClick(id) {
-    setTasks((prevTasks) => {
-      return prevTasks.filter((item, index) => {
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
     });
   }
 
-  function handleAdd(event) {
-    setTasks([
-      ...tasks,
-      {
-        title: event.target[0].value,
-        content: event.target[1].value
-      }
-    ]);
-  }
-
   return (
     <div>
+      <TipsAndUpdatesIcon />
       <Header />
-      <CreateArea runthis={handleAdd} />
-      {tasks.map((task, index) => (
-        <Note
-          key={index}
-          id={index}
-          title={task.title}
-          content={task.content}
-          handleClick={() => handleClick(index)}
-        />
-      ))}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,17 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
 
 function CreateArea(props) {
-  function handleSubmit(event) {
-    props.runthis(event);
-    event.target[0].value = event.target[1].value = "";
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    console.log(name);
+    console.log(value);
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  const [Show, setShow] = useState("none");
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    setShow("none");
     event.preventDefault();
   }
+
+  function expand() {
+    setShow(null);
+  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button type="submit">Add</button>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+          onClick={expand}
+        />
+        <div style={{ display: Show }}>
+          <textarea
+            name="content"
+            onChange={handleChange}
+            value={note.content}
+            placeholder="Take a note..."
+            rows="3"
+          />
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </div>
       </form>
     </div>
   );
